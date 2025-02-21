6a9f1eea8742895d9a61c73d068d80d98a1f48b7
Timothy Ku <timothykukt@gmail.com>
2025-02-20 18:48:48 -0600

    created a DAOfor User

--- Expense-Reimbursement-System-Backend/src/main/java/com/revature/DAOs/UserDAO.java
+++ Expense-Reimbursement-System-Backend/src/main/java/com/revature/DAOs/UserDAO.java
@@ -1,4 +1,17 @@
 package com.revature.DAOs;
 
-public interface UserDAO {
+import com.revature.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+//Extending JpaRepository gives us access to a bunch of basic CRUD methods. We don't have to write them!
+    //This includes find all, find by id, insert, update, delete
+
+//JpaRepository takes 2 generics:
+    //1. The entity we're working with, which is "User"
+    //2. The type of the primary key (UserId), which is Integer
+@Repository //1 of the 4 stereotype annotations (make a class a bean)
+public interface UserDAO extends JpaRepository<User, Integer> {
+
+
 }
