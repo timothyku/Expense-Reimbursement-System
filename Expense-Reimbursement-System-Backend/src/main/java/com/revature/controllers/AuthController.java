f534e6347d908a276558771b052c22a73bfff60e
Timothy Ku <timothykukt@gmail.com>
2025-02-22 17:10:51 -0600

    handled login from the backend

--- Expense-Reimbursement-System-Backend/src/main/java/com/revature/controllers/AuthController.java
+++ Expense-Reimbursement-System-Backend/src/main/java/com/revature/controllers/AuthController.java
@@ -1,8 +1,10 @@
 package com.revature.controllers;
 
+import com.revature.models.DTOs.LoginDTO;
 import com.revature.models.DTOs.OutgoingUserDTO;
 import com.revature.models.User;
 import com.revature.services.AuthService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -29,6 +31,42 @@ public class AuthController {
 
         //Send the inserted User back to the client in a response
         return ResponseEntity.ok(returnedUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<OutgoingUserDTO> login(@RequestBody LoginDTO loginDTO, HttpSession session){
+
+        //Note: we have an HttpSession coming in through parameters, implicitly included in every HTTP request
+        //Login is where we set it up
+
+        //Try to login (send the loginDTO to the service)
+        OutgoingUserDTO loggedInUser = authService.login(loginDTO);
+        //If anything goes wrong, the service throws an exception and our global Exception handler takes over
+
+        //If we get here, the login was successful - we can build up the User's session
+        session.setAttribute("userId", loggedInUser.getUserId());
+        session.setAttribute("username", loggedInUser.getUsername());
+        session.setAttribute("role", loggedInUser.getRole());
+
+        //It's really easy to access these values with getAttribute()!
+        System.out.println("User " + session.getAttribute("username") + " has logged in!");
+
+        //Set session timeout to 24 hours
+        //session.setMaxInactiveInterval(24 * 60 * 60);
+
+        /*WHY store all this info in a Session?
+
+          -It lets us store user info that can be used for checks throughout the app
+            -check that user is logged in (session != null)
+            -check that a user's role is appropriate (role.equals("manager")
+            -personalize the app (user the user's name in HTTP responses to use them in the UI etc.)
+            -simplify our URLs
+                -e.g. use the stored userID in "findSomethingByUserId" methods instead of sending it in the PATH
+                -This cleans up our URLs and secures them a bit more too.
+        */
+
+        //Return the User info to the Client
+        return ResponseEntity.ok(loggedInUser);
 
     }
 }
---
+++ Expense-Reimbursement-System-Backend/src/main/java/com/revature/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.revature.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+//We can use a global exception handler to clean up our code
+//Would you rather:
+//Have a try/catch in every single controller for the exception thrown in service?
+//OR... have clean controllers and handle all exception stuff here (Preferred)
+
+@RestControllerAdvice //This lets our handler intercept exceptions thrown in any controller
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+
+        //401 == UNAUTHORIZED - good for auth errors
+        //we send the message in the exception in the response body (should change based on what went wrong)
+    }
+
+}
